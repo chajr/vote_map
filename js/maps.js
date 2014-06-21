5d99df8cee6190a8b7a016cf064fe78b10f541f3
chajr <git@bluetree.pl>
2014-06-21 20:33:31 +0200

    geolocation and shape fixes
    This version introduces multiple enhancements:
    
    added possibility to check search location by address and also check that given location exist in created shape
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   /index.html
                -- Added --
                    form to search given address and info boxes
                -- Updated --
                    fixed script paths and links
    
    Modified:   /js/maps.js
                -- Added --
                    added geocoding and show info box
                -- Updated --
                    some refactor changes
    
    Modified:   /css/maps.css
                -- Added --
                    styles fon new changes

--- js/maps.js
+++ js/maps.js
@@ -1,47 +1,47 @@
+var mainPoint;
+var contentString1 = 'some content to display';
+var mapOptions;
+var map;
+var marker1;
+var infoWindow1;
+var testArea;
+var geocoder;
+var area = [
+    new google.maps.LatLng(50.577915, 19.294691),
+    new google.maps.LatLng(50.583256, 19.322157),
+    new google.maps.LatLng(50.577043, 19.341211),
+    new google.maps.LatLng(50.563741, 19.353056),
+    new google.maps.LatLng(50.552618, 19.318380),
+    new google.maps.LatLng(50.558180, 19.298467)
+];
+
 function initialize() {
-    var mainPoint       =  new google.maps.LatLng(50.570452366163686, 19.306543171405792);
-    var otherPoint      =  new google.maps.LatLng(50.590667, 19.353914);
-    var contentString   = 'some content to display';
-    var mapOptions      = {
+    mainPoint       = new google.maps.LatLng(50.570452366163686, 19.306543171405792);
+    geocoder        = new google.maps.Geocoder();
+    mapOptions      = {
         center: mainPoint,
         zoom: 12
     };
-    var map = new google.maps.Map(
+    map = new google.maps.Map(
         document.getElementById("map-canvas"),
         mapOptions
     );
 
-    var marker = new google.maps.Marker({
+    marker1 = new google.maps.Marker({
         position: mainPoint,
         map: map,
-        title:"Main point"
-        //,icon: 'url'
-    });
-    var marker2 = new google.maps.Marker({
-        position: otherPoint,
-        map: map,
-        title:"Other point"
-        //,icon: 'url'
+        title:"Main point",
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
     });
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
+    infoWindow1 = new google.maps.InfoWindow({
+        content: contentString1
     });
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
+    google.maps.event.addListener(marker1, 'click', function() {
+        infoWindow1.open(map, marker1);
     });
 
-    var area = [
-        new google.maps.LatLng(50.577915, 19.294691),
-        new google.maps.LatLng(50.583256, 19.322157),
-        new google.maps.LatLng(50.577043, 19.341211),
-        new google.maps.LatLng(50.563741, 19.353056),
-        new google.maps.LatLng(50.552618, 19.318380),
-        new google.maps.LatLng(50.558180, 19.298467)
-        
-        
-    ];
-    var testArea = new google.maps.Polygon({
+    testArea = new google.maps.Polygon({
         paths: area,
         strokeColor: '#FF0000',
         strokeOpacity: 0.8,
@@ -51,4 +51,34 @@ function initialize() {
     });
     testArea.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$('#submit').click(function()
+{
+    $('.success').hide();
+    $('.error').hide();
+    var address = $('#address').val();
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+            var exist = google.maps.geometry.poly.containsLocation(
+                results[0].geometry.location,
+                testArea
+            );
+
+            if (exist) {
+                infoWindow1.open(map, marker1);
+                $('.success').show();
+            } else {
+                $('.error').show();
+            }
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+});
